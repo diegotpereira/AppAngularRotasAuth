3559074bb433db9c05d3474020a2cce86e74d689
Diego <diegoteixeirapereira@hotmail.com>
2021-03-23 17:23:07 -0300

    alteração nos arquivos app.component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    Error404Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
